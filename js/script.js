f17b226b79cdc2ff967386ec06e7c92f5fa49a6a
Matteo Ponchietti <matteoponchietti@gmail.com>
2020-10-27 09:02:41 +0000

    FIX stamp dichiarata

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var boxResultEl = document.getElementById('boxResult');
 
 var div = '<div>';
 var divCl = '</div>'
-
+var stamp = '';
 for (var i = 1; i <= 100; i++){
   if (i % 3 == 0 && i % 5 == 0 ){
     stamp = 'FizzBuzz';
